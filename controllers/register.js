aa150acc9e2cb32f59e4953d19e605286f001f5d
jrios002 <32584435+jrios002@users.noreply.github.com>
2020-05-03 10:51:09 -0500

    try to catch errors

--- controllers/register.js
+++ controllers/register.js
@@ -25,6 +25,6 @@ const handleRegister = (req, res, db, bcrypt) => {
 		.then(trx.commit)
 		.catch(trx.rollback)
 	})
-	.catch(err => res.status(400).json('unable to register'))
+	.catch(err => console.log(err))
 }
 module.exports = {handleRegister: handleRegister};
