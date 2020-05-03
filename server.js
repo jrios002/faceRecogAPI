538b07b50c0ff498ceea500ba0bfc4dc68a21ef4
jrios002 <32584435+jrios002@users.noreply.github.com>
2020-05-02 23:19:25 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+const register = require('./controllers/register');
+const signIn = require('./controllers/signIn');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+	client: 'pg',
+	connection: {
+		host: '127.0.0.1',
+		user: 'postgres',
+		password: '123',
+		database: 'postgres'
+	}
+});
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req, res)=> {res.send(db.users);});
+
+app.post('/imageUrl', (req, res) => {image.handleApiCall(req, res)});
+app.post('/signIn', (req, res) => {signIn.handleSignIn(req, res, db, bcrypt)});
+app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)});
+app.get('/profile/:id', (req, res) => {profile.handleProfile(req, res, db)});
+app.put('/image', (req, res) => {image.handleImage(req, res, db)});
+
+app.listen(3001, ()=> {
+	console.log('app is running on port 3001');
+});
