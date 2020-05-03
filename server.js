c19500faccf49777c5758c48e18fc205ea98e1f3
jrios002 <32584435+jrios002@users.noreply.github.com>
2020-05-03 10:23:57 -0500

    updating database

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const image = require('./controllers/image');
 const db = knex({
 	client: 'pg',
 	connection: {
-		host: '127.0.0.1',
+		host: 'postgresql-spherical-87913',
 		user: 'postgres',
 		password: '123',
 		database: 'postgres'
