cd8675f7020a7e102fe3e4e76af9e2b93f29a99c
jrios002 <32584435+jrios002@users.noreply.github.com>
2020-05-03 10:30:48 -0500

    fix connectionString

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const image = require('./controllers/image');
 const db = knex({
 	client: 'pg',
 	connection: {
-		host: 'postgresql-spherical-87913',
+		connectionString: process.env.DATABASE_URL,
 		ssl: true
 	}
 });
