7c9af133fbdaf0def2b8d4ed009b4fc9da406887
jrios002 <32584435+jrios002@users.noreply.github.com>
2020-05-03 11:06:19 -0500

    fix connection issues

--- controllers/register.js
+++ controllers/register.js
@@ -1,6 +1,3 @@
-const cors = require('cors');
-app.use(cors());
-
 const handleRegister = (req, res, db, bcrypt) => {
 	const { email, name, password } = req.body;
 	const hash = bcrypt.hashSync(password);
